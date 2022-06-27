7b5d94660070b004147044dbd927aad22eea3a0f
İbrahim Duman <dumanibrahm@gmail.com>
2022-06-27 23:59:33 +0300

    Delegate and DataSource connections are made.

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let data = ["Swift","Kotlin","C++","C","Phyton","Java","C#"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text=data[indexPath.row]
+        return cell
+    }
 
 }
 
