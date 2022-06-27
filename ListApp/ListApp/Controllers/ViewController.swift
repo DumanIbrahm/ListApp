37f35febba0e0526400505d9a51b80950f262143
İbrahim Duman <dumanibrahm@gmail.com>
2022-06-27 23:33:04 +0300

    Project created

---
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by İbrahim Duman on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
